1c295e1033afa425f18f12c7ffba570f89c76c89
Snehap@1322 <snehapurshotham@gmail.com>
2026-09-25 11:49:30 +0530

    conversation added

--- kanna.sh
+++ kanna.sh
@@ -7,4 +7,9 @@ B=sneha
 
 echo "$B:: hello $A"
 echo "$A:: hello $B"
+#this is mobaxterm editing
 
+echo "$A:: Are you comming tomorrow to Bengaluru $B"
+echo "$B:: yes $A I will be there"
+
+#this is VSD editing
